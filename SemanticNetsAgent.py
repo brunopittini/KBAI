77a2b7c2cac836b65db98d09c403ec7658f18f7f
Bruno Pittini <brunopittini@knights.ucf.edu>
2022-01-29 18:05:08 -0500

    Complete state class and BFS function

--- SemanticNetsAgent.py
+++ SemanticNetsAgent.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class SemanticNetsAgent:
     def __init__(self):
         #If you want to do any initial processing, add it here.
@@ -13,5 +15,42 @@ class SemanticNetsAgent:
         #If it is impossible to move the animals over according
         #to the rules of the problem, return an empty list of
         #moves.
-        pass
 
+        class State():
+            
+            def __init__(self, sheep_left, wolves_left, sheep_right, wolves_right, boat_loc):
+                self.sheep_left = sheep_left
+                self.wolves_left = wolves_left
+                self.sheep_right = sheep_right
+                self.wolves_right = wolves_right
+                self.boat_loc = boat_loc
+            
+            def is_goal(self):
+                pass
+            
+            def is_valid(self):
+                pass
+
+            def generate(current_state):
+                pass
+
+            def ret_path(self):
+                pass
+
+            def BFS(self):
+                initial_state = State(initial_sheep, initial_wolves, 0, 0, 0)
+                if initial_state.is_goal():
+                    return initial_state
+                frontier = []
+                frontier.append(initial_state)
+                explored = set()
+                while frontier:
+                    state = frontier.pop(0)
+                    if state.is_goal():
+                        return state
+                    explored.append(state)
+                    children = generate(state)
+                    for child in children:
+                        if (child not in explored) and (child not in frontier):
+                            frontier.append(child)
+                return None
